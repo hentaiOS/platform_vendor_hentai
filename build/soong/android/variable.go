519f43968a160532eb535418ffeee5384dbbb56c
Jan Altensen <info@stricted.net>
2019-09-12 06:50:49 +0200

    soong_config: add TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE
    Change-Id: I96dd2f7dd629a06de288332e1d00be827029e89c

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,8 +3,12 @@ type Product_variables struct {
 	Additional_gralloc_10_usage_bits struct {
 		Cppflags []string
 	}
+	Target_needs_netd_direct_connect_rule struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
+	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -4,6 +4,7 @@ _json_contents := $(_json_contents)    "Hentai":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_bool, Target_needs_netd_direct_connect_rule, $(filter true,$(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
