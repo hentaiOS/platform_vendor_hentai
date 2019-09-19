b655f2fd592d9968d3d451ed1e909687e8ca0fd5
Michael Bestas <mkbestas@lineageos.org>
2019-09-20 01:15:25 +0300

    soong: Add AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT
    Change-Id: Iac6366d714ab3634def648e6a8c0c30d684a9383

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,9 +6,13 @@ type Product_variables struct {
 	Target_init_vendor_lib struct {
 		Whole_static_libs []string
 	}
+	Supports_extended_compress_format struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
 	Target_init_vendor_lib  *string `json:",omitempty"`
+	Supports_extended_compress_format  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -5,6 +5,7 @@ _json_contents := $(_json_contents)    "Hentai":{$(newline)
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
 $(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
+$(call add_json_bool, Supports_extended_compress_format, $(filter true,$(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
