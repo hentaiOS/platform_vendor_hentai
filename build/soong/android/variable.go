d327e739c3253af7af8143d79e16059c9ccbfb01
Kevin Haggerty <haggertk@lineageos.org>
2019-11-25 05:00:48 -0500

    soong_config: Allow extension of valid gralloc 1.0 buffer usage bits
    * fw/native change I7f4174581e24e361577640b9263514a168ed482d
      implemented validation of the buffer description info prior to
      creating the descriptor. Some of our legacy devices need to
      whitelist additional usage bits to support various functionality.
    * The TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS variable can contain
      a singular roll-up value (e.g., 0x02000000U), a left-shifted bit
      (e.g., (1 << 25)), a bitwise OR of these things, or essentially
      anything that the compiler can choke down.
    
    Change-Id: I2127d33b03426b5e0f981aae837e07d82163fa17

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,8 +1,10 @@
 package android
 type Product_variables struct {
-
+	Additional_gralloc_10_usage_bits struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
-
+	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,7 +1,7 @@
 _json_contents := $(_json_contents)    "Hentai":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
-
+$(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
