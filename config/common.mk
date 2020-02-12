e2c6808a96719634a0b9c5d702b5362a583bbfef
Arseniy Graur <argraur@aosip.dev>
2020-02-12 11:06:55 +0000

    hentai: Import PixelConfigOverlay* GoogleConfigOverlay GoogleWebViewOverlay for full GMS support
    Signed-off-by: Arseniy Graur <argraur@aosip.dev>

--- config/common.mk
+++ config/common.mk
@@ -18,6 +18,42 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/hentai/apex
 
+# Gboard configuration
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.ime.bs_theme=true \
+    ro.com.google.ime.theme_id=5 \
+    ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
+
+# SetupWizard configuration
+PRODUCT_PRODUCT_PROPERTIES += \
+    setupwizard.feature.baseline_setupwizard_enabled=true \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.rotation_locked=true \
+    setupwizard.enable_assist_gesture_training=true \
+    setupwizard.theme=glif_v3_light \
+    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
+    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
+    setupwizard.feature.show_pixel_tos=false
+
+# StorageManager configuration
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.storage_manager.show_opt_in=false
+
+# OPA configuration
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.opa.eligible_device=true
+
+# Google legal
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html
+
+# Google Play services configuration
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.clientidbase=android-google \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent
+
 PRODUCT_PACKAGES += \
     ModuleMetadataGooglePrebuilt
 
@@ -29,3 +65,6 @@ $(call inherit-product, vendor/hentai/config/fonts.mk)
 
 # Inherit from our versioning
 $(call inherit-product, vendor/hentai/config/versioning.mk)
+
+# Inherit from GMS product config
+$(call inherit-product, vendor/gms/gms_full.mk)
