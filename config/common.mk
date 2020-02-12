47acb8706f0cd83dae9b45a68af86b0f46bb861d
Arseniy Graur <argraur@aosip.dev>
2020-02-12 09:52:38 +0000

    hentai: Add fonts for customization and set Google Sans as default font.
    Signed-off-by: Arseniy Graur <argraur@aosip.dev>

--- config/common.mk
+++ config/common.mk
@@ -21,5 +21,11 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     ModuleMetadataGooglePrebuilt
 
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/hentai/overlay
+
+# Inherit from fonts config
+$(call inherit-product, vendor/hentai/config/fonts.mk)
+
 # Inherit from our versioning
 $(call inherit-product, vendor/hentai/config/versioning.mk)
---
+++ config/fonts.mk
@@ -0,0 +1,62 @@
+#
+# Copyright (C) 2018-2019 The Pixel3ROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# arbutus-slab family
+PRODUCT_PACKAGES += \
+    ArbutusSlab-Regular.ttf
+
+# google-sans family
+PRODUCT_PACKAGES += \
+    GoogleSans-BoldItalic.ttf \
+    GoogleSans-Bold.ttf \
+    GoogleSans-Italic.ttf \
+    GoogleSans-MediumItalic.ttf \
+    GoogleSans-Medium.ttf \
+    GoogleSans-Regular.ttf
+
+# lato family
+PRODUCT_PACKAGES += \
+    Lato-BoldItalic.ttf \
+    Lato-Bold.ttf \
+    Lato-Italic.ttf \
+    Lato-MediumItalic.ttf \
+    Lato-Medium.ttf \
+    Lato-Regular.ttf
+
+# rubik family
+PRODUCT_PACKAGES += \
+    Rubik-BoldItalic.ttf \
+    Rubik-Bold.ttf \
+    Rubik-Italic.ttf \
+    Rubik-MediumItalic.ttf \
+    Rubik-Medium.ttf \
+    Rubik-Regular.ttf
+
+# zilla-slab family
+PRODUCT_PACKAGES += \
+    ZillaSlab-MediumItalic.ttf \
+    ZillaSlab-Medium.ttf \
+    ZillaSlab-SemiBoldItalic.ttf \
+    ZillaSlab-SemiBold.ttf
+
+# Customization overlays
+PRODUCT_PACKAGES += \
+    FontArbutusSourceOverlay \
+    FontArvoLatoOverlay \
+    FontRubikRubikOverlay
+
+PRODUCT_COPY_FILES += \
+    vendor/hentai/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
