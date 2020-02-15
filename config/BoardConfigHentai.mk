723a026a067b9dd1256044819f504c511446c27f
Arseniy Graur <argraur@aosip.dev>
2020-02-15 21:15:00 +0000

    [1/2] hentai: Add a guard for all kernel-related tasks to check for prebuilt kernel
    * Do not use generated_kernel_headers if prebuilt kernel
    * Do not post tasks in kernel.mk if prebuilt kernel
    
    Signed-off-by: Arseniy Graur <argraur@aosip.dev>

--- config/BoardConfigHentai.mk
+++ config/BoardConfigHentai.mk
@@ -1,5 +1,9 @@
 # Kernel
+ifeq ($(LOCAL_KERNEL),)
 include vendor/hentai/config/BoardConfigKernel.mk
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/hentai/build/soong/generator
+endif
 
 # Qcom-specific bits
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
