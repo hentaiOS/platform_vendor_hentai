e49690e6974784f08d772d229927cd80a003f6a1
Dyneteve <sound0020@gmail.com>
2020-02-16 08:18:13 +0000

    hentai: soong: Clean things up

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,18 +1,14 @@
 package android
 type Product_variables struct {
-	Target_init_vendor_lib struct {
-		Whole_static_libs []string
-	}
 	Additional_gralloc_10_usage_bits struct {
 		Cppflags []string
 	}
-	Target_needs_netd_direct_connect_rule struct {
-		Cppflags []string
+	Target_init_vendor_lib struct {
+		Whole_static_libs []string
 	}
 }
 
 type ProductVariables struct {
-	Target_init_vendor_lib  *string `json:",omitempty"`
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
-	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
+	Target_init_vendor_lib  *string `json:",omitempty"`
 }
