d57611417ee4930f9a6c87fcb645a86de23d49b2
dianlujitao <dianlujitao@lineageos.org>
2020-05-25 18:08:31 +0800

    hentai: build: soong: Export qcom UM hal root to soong
    Change-Id: Iabb75e8745dba9069c2eab0d5ef41b2171f4c048

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -9,10 +9,15 @@ type Product_variables struct {
 	Supports_extended_compress_format struct {
 		Cflags []string
 	}
+	Qcom_um_soong_namespace struct {
+		Cflags []string
+		Header_libs []string
+	}
 }
 
 type ProductVariables struct {
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
 	Target_init_vendor_lib  *string `json:",omitempty"`
 	Supports_extended_compress_format  *bool `json:",omitempty"`
+	Qcom_um_soong_namespace  *string `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -6,6 +6,7 @@ _json_contents := $(_json_contents)    "Hentai":{$(newline)
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
 $(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
 $(call add_json_bool, Supports_extended_compress_format, $(filter true,$(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)))
+$(call add_json_str_omitempty, Qcom_um_soong_namespace, $(if $(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),$(QCOM_SOONG_NAMESPACE),))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
