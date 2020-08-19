1aa7cda222953939a7708dfa56e309217f43a192
Dyneteve <dyneteve@hentaios.com>
2020-08-19 16:49:19 +0200

    hentai: build: Move hentai security out of vendor

--- build/target/product/security/generate-keys.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Example of creating hentai subject file
-# $ '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com' > hentai
-
-export hentai=`cat hentai`
-for x in releasekey platform shared media; do \
-    ../../../../../../development/tools/make_key ./$x "$hentai"; \
-done
