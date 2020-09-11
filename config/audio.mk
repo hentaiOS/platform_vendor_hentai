b0b5354dfddd234e1d34f91005b01ddd435dee78
Dyneteve <dyneteve@hentaios.com>
2020-09-11 19:49:36 +0200

    Revert "hentai: config: Move the sound proprieties to product"
    * Apparently it only works in vendor
    
    This reverts commit 9f81c3f5619f7afbdf79917fea630f359054729d.

--- config/audio.mk
+++ config/audio.mk
@@ -82,7 +82,7 @@ PRODUCT_COPY_FILES += \
     vendor/hentai/audio/ui/WirelessChargingStarted.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/WirelessChargingStarted.ogg
 
 # Change default sounds
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=The_big_adventure.ogg \
     ro.config.notification_sound=Popcorn.ogg \
     ro.config.alarm_alert=Bright_morning.ogg
