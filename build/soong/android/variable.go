5f8329be9254626e680cdcc96b7f694d95e18935
Dyneteve <dyneteve@hentaios.com>
2020-10-06 08:46:53 +0200

    hentai: build: soong: Drop init/gralloc usage bits

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,11 +1,5 @@
 package android
 type Product_variables struct {
-	Additional_gralloc_10_usage_bits struct {
-		Cppflags []string
-	}
-	Target_init_vendor_lib struct {
-		Whole_static_libs []string
-	}
 	Supports_extended_compress_format struct {
 		Cflags []string
 	}
@@ -16,8 +10,6 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
-	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
-	Target_init_vendor_lib  *string `json:",omitempty"`
 	Supports_extended_compress_format  *bool `json:",omitempty"`
 	Qcom_um_soong_namespace  *string `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -3,8 +3,6 @@ add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 _json_contents := $(_json_contents)    "Hentai":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
-$(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
-$(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
 $(call add_json_bool, Supports_extended_compress_format, $(filter true,$(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)))
 $(call add_json_str_omitempty, Qcom_um_soong_namespace, $(if $(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),$(QCOM_SOONG_NAMESPACE),))
 
