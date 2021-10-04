fb5be49ec11c92b2ad096a2492579560a000c4af
Dyneteve <dyneteve@hentaios.com>
2021-10-04 22:12:42 +0200

    hentai: rro_overlays: Update from SP1A.210812.016
    Change-Id: Idd547f7c5d0a2cf4509d3254346f854f272232ea

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -45,11 +45,13 @@ PRODUCT_PACKAGES += \
 
 # Google Overlays
 PRODUCT_PACKAGES += \
+    CaptivePortalLoginOverlay \
     CellBroadcastServiceOverlay \
-    FilesOverlay \
     GoogleConfigOverlay \
     GooglePermissionControllerOverlay \
     GoogleWebViewOverlay \
+    ManagedProvisioningPixelOverlay \
+    ManagedProvisioningPixelOverlay \
     PixelConfigOverlay2018 \
     PixelConfigOverlay2019 \
     PixelConfigOverlay2019Midyear \
