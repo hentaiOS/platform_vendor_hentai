72bdad130bb9e6f562b43bc15147f4023f03090b
Dyneteve <dyneteve@hentaios.com>
2021-10-06 15:32:25 +0200

    hentai: build: product: Reflactor
    Change-Id: I8232d3fb830bf39bd164d8824eb7de2ad12732ae

--- build/product/hentai_product.mk
+++ build/product/hentai_product.mk
@@ -14,119 +14,26 @@
 # limitations under the License.
 #
 
-# This makefile is the basis of a generic system image for a handheld device.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
-
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit hentai products.
-$(call inherit-product, vendor/hentai/build/product/handheld_system.mk)
 $(call inherit-product, vendor/hentai/config/common_telephony.mk)
 
-# Shared java libs
+# Includes all AOSP product packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
+
+# Additional settings used in all AOSP builds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.android.dataroaming?=false \
+
+# More AOSP packages
 PRODUCT_PACKAGES += \
-    com.android.nfc_extras
+    messaging \
+    PhotoTable \
+    preinstalled-packages-platform-aosp-product.xml \
+    WallpaperPicker \
 
-# Framework overlays
-PRODUCT_PACKAGES += \
-    frameworks-base-overlays
-
-# Debug framework overlays
-PRODUCT_PACKAGES_DEBUG += \
-    frameworks-base-overlays-debug
-
-# Applications
-PRODUCT_PACKAGES += \
-    LiveWallpapersPicker \
-    PartnerBookmarksProvider \
-    PresencePolling \
-    QuickAccessWallet \
-    RcsService \
-    SafetyRegulatoryInfo \
-    Stk \
-    TimeZoneUpdater
-
-# Binaries
-PRODUCT_PACKAGES += llkd
-
-# OTA support
-PRODUCT_PACKAGES += \
-    recovery-refresh \
-    update_engine \
-    update_verifier
-
-# Wrapped net utils for /vendor access.
-PRODUCT_PACKAGES += netutils-wrapper-1.0
-
-# Charger images
-PRODUCT_PACKAGES += charger_res_images
-
-# system_other support
-PRODUCT_PACKAGES += \
-    cppreopts.sh \
-    otapreopt_script
-
-# Bluetooth libraries
-PRODUCT_PACKAGES += \
-    audio.a2dp.default \
-    audio.hearing_aid.default
-
-# For ringtones that rely on forward lock encryption
-PRODUCT_PACKAGES += libfwdlockengine
-
-# System libraries commonly depended on by things on the system_ext or product partitions.
-# These lists will be pruned periodically.
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1 \
-    android.hardware.radio@1.0 \
-    android.hardware.radio@1.1 \
-    android.hardware.radio@1.2 \
-    android.hardware.radio@1.3 \
-    android.hardware.radio@1.4 \
-    android.hardware.radio.config@1.0 \
-    android.hardware.radio.deprecated@1.0 \
-    android.hardware.secure_element@1.0 \
-    android.hardware.wifi@1.0 \
-    libaudio-resampler \
-    libaudiohal \
-    libdrm \
-    liblogwrap \
-    liblz4 \
-    libminui \
-    libnl \
-    libprotobuf-cpp-full
-
-# These libraries are empty and have been combined into libhidlbase, but are still depended
-# on by things off /system.
-# TODO(b/135686713): remove these
-PRODUCT_PACKAGES += \
-    libhidltransport \
-    libhwbinder
-
-PRODUCT_PACKAGES_DEBUG += \
-    avbctl \
-    bootctl \
-    tinyplay \
-    tinycap \
-    tinymix \
-    tinypcminfo \
-    update_engine_client
-
-PRODUCT_HOST_PACKAGES += \
-    tinyplay
-
-# Include all zygote init scripts. "ro.zygote" will select one of them.
-PRODUCT_COPY_FILES += \
-    system/core/rootdir/init.zygote32.rc:system/etc/init/hw/init.zygote32.rc \
-    system/core/rootdir/init.zygote64.rc:system/etc/init/hw/init.zygote64.rc \
-    system/core/rootdir/init.zygote32_64.rc:system/etc/init/hw/init.zygote32_64.rc \
-    system/core/rootdir/init.zygote64_32.rc:system/etc/init/hw/init.zygote64_32.rc
-
-# Put en_US first in the list, so make it default.
-PRODUCT_LOCALES := en_US
-
-# Enforce RRO targetsd
+# Enforce RRO targets
 PRODUCT_ENFORCE_RRO_TARGETS := *
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_default.mk)
