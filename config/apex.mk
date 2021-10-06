a38527cb600fc7eb1b646fa04db31af021c141e7
Dyneteve <dyneteve@hentaios.com>
2021-10-06 15:32:16 +0200

    hentai: apex: Specify that we don't use AOSP ART
    Change-Id: I8befec03aa2dd6d8af36336abaea5e205906d037

--- config/apex.mk
+++ config/apex.mk
@@ -17,6 +17,10 @@
 # Networkstack certificate
 PRODUCT_MAINLINE_SEPOLICY_DEV_CERTIFICATES := vendor/hentai/apex/NetworkStack
 
+# Prebuilt module SDKs require prebuilt modules to work, and currently
+# prebuilt modules are only provided for com.google.android.xxx.
+MODULE_BUILD_FROM_SOURCE := false
+
 # Enable Google Play system updates support
 PRODUCT_SOONG_NAMESPACES += \
     vendor/hentai/apex
