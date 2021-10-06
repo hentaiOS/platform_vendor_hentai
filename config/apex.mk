74204c3e6f918d19b825dca54f37d76adeb076f8
Dyneteve <dyneteve@hentaios.com>
2021-10-06 13:57:45 +0200

    hentai: apex: Update from SP1A.210812.016
    Change-Id: I725c010e4064305b4c387c63eb07c3e33f3d7dbb

--- config/apex.mk
+++ config/apex.mk
@@ -28,6 +28,8 @@ PRODUCT_PACKAGES += \
 # Google Apexes
 PRODUCT_PACKAGES += \
     com.google.android.adbd \
+	com.google.android.appsearch \
+	com.google.android.art \
 	com.google.android.cellbroadcast \
 	com.google.android.conscrypt \
 	com.google.android.extservices \
@@ -39,8 +41,10 @@ PRODUCT_PACKAGES += \
 	com.google.android.os.statsd \
 	com.google.android.permission \
 	com.google.android.resolv \
+	com.google.android.scheduling \
 	com.google.android.sdkext \
 	com.google.android.telephony \
 	com.google.android.tethering \
-	com.google.android.tzdata2 \
-	com.google.android.wifi
+	com.google.android.tzdata3 \
+	com.google.android.wifi \
+	com.google.mainline.primary.libs
