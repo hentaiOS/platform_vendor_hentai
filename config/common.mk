b7e80d86d62f347d0b827f92453efa11c8527795
Dyneteve <dyneteve@hentaios.com>
2021-10-31 20:36:55 +0100

    hentai: config: Import SetupWizard/IME props from SD1A.210817.015.A4

--- config/common.mk
+++ config/common.mk
@@ -16,7 +16,6 @@
 
 # Gboard configuration
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.ime.bs_theme=true \
     ro.com.google.ime.theme_id=5 \
     ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
 
@@ -26,9 +25,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.setupwizard.esim_cid_ignore=00000001 \
     ro.setupwizard.rotation_locked=true \
     setupwizard.feature.baseline_setupwizard_enabled=true \
-    setupwizard.feature.device_default_dark_mode=true \
+    setupwizard.feature.day_night_mode_enabled=true \
+    setupwizard.feature.portal_notification=true \
     setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
     setupwizard.feature.show_pixel_tos=true \
+    setupwizard.feature.show_support_link_in_deferred_setup=false \
     setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
     setupwizard.theme=glif_v3_light
 
