d864dd48290c5ff2f0a1a7ad0bbe47393322dd1f
Dyneteve <dyneteve@hentaios.com>
2022-07-18 20:49:28 +0200

    hentai: build: Deprecrate building kernel inline
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>
    Change-Id: I6ed51793dc43dfe6352f1aa8b31dfef540a83192

--- config/BoardConfigHentai.mk
+++ config/BoardConfigHentai.mk
@@ -1,8 +1,3 @@
-# Kernel
-ifeq ($(LOCAL_KERNEL),)
-include vendor/hentai/config/BoardConfigKernel.mk
-endif
-
 # Qcom-specific bits
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/hentai/config/BoardConfigQcom.mk
