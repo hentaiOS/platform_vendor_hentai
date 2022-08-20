94313ef3a64368d697d6f631186a065a22dd74ec
Dyneteve <dyneteve@hentaios.com>
2022-08-20 13:15:06 +0200

    hentai: config: Bump to TwistedScarlett
    Change-Id: Ie9c4f69af77aefce39c661cf79b0b1a45d4efb7b

--- config/versioning.mk
+++ config/versioning.mk
@@ -17,7 +17,7 @@
 
 BUILD_ID_LC ?= $(shell echo $(BUILD_ID) | tr '[:upper:]' '[:lower:]')
 
-PLATFORM_HENTAI_RELEASE := SaltyIceCream
+PLATFORM_HENTAI_RELEASE := TwistedScarlett
 
 ifeq ($(TARGET_BUILD_VARIANT),user)
     PLATFORM_HENTAI_VERSION := $(PLATFORM_HENTAI_RELEASE).Furry.$(shell date +%m%d%H%M)
