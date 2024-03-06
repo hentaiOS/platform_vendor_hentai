f344257f998aedc5b47cdad4b5f8ae36b2e101e5
Raphiel Rollerscaperers <rapherion@raphielgang.org>
2024-03-07 03:01:40 +0700

    Update copyright to 2024
    Change-Id: I8dde1fec59b5ff8b323eb2ade5beed38ec01be5c
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

--- config/gfonts.mk
+++ config/gfonts.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2018-2019 The Pixel3ROM Project
-# Copyright (C) 2021 Raphielscape LLC.
+# Copyright (C) 2024 The hentaiOS Project and its Proprietors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
