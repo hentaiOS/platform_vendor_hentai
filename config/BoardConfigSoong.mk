bd792b7816f6d4b2e31fa79bf462bd198a23d5e1
Dyneteve <dyneteve@hentaios.com>
2024-10-19 13:01:40 +0000

    build: Conditionally spoof ro.product.first_api_level
    Also clean up the unused fod lib properties and kernel exports.
    
    Change-Id: Ia566fa810df97336105cecc6ba23a73667761a50
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>
    Co-authored-by: AnierinB <anierin@evolution-x.org>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -1,16 +1,5 @@
 PATH_OVERRIDE_SOONG := $(shell echo $(TOOLS_PATH_OVERRIDE))
 
-# Add variables that we wish to make available to soong here.
-EXPORT_TO_SOONG := \
-    KERNEL_ARCH \
-    KERNEL_BUILD_OUT_PREFIX \
-    KERNEL_CROSS_COMPILE \
-    KERNEL_MAKE_CMD \
-    KERNEL_MAKE_FLAGS \
-    PATH_OVERRIDE_SOONG \
-    TARGET_KERNEL_CONFIG \
-    TARGET_KERNEL_SOURCE
-
 # Setup SOONG_CONFIG_* vars to export the vars listed above.
 # Documentation here:
 # https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
@@ -28,10 +17,7 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += hentaiGlobalVars
 SOONG_CONFIG_hentaiGlobalVars += \
-    target_surfaceflinger_fod_lib
-
-# Set default values
-TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
+    spoof_first_api_level_32
 
 # Soong value variables
-SOONG_CONFIG_hentaiGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
+SOONG_CONFIG_hentaiGlobalVars_spoof_first_api_level_32 := $(BOARD_SPOOF_FIRST_API_LEVEL_32)
